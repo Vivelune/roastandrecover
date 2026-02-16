d00e0f4d2258caf931220dbf9feadfe91b30e1de
Vivelune <shafiqueeee.atif@gmail.com>
2026-02-16 21:50:50 +0800

    12:Added Header Containing Logo, Login, Order, Cartitems

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,4 +1,7 @@
+import { Header } from '@/components/featuredSections/Header'
+import { Toaster } from '@/components/ui/sonner'
 import { CartStoreProvider } from '@/lib/store/cart-store-provider'
+import { ChatStoreProvider } from '@/lib/store/chat-store-provider'
 import { SanityLive } from '@/sanity/lib/live'
 import { ClerkProvider } from '@clerk/nextjs'
 import React from 'react'
@@ -7,8 +10,12 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <CartStoreProvider>
+        <ChatStoreProvider>
+        <Header/>
     <main>{children}</main>
+    <Toaster position="top-center"/>
     <SanityLive/>
+    </ChatStoreProvider>
     </CartStoreProvider>
     </ClerkProvider>
   )
