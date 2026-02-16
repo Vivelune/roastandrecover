a66dac319ae49fe0e6429cb7ebf7d0d0dbcca980
Vivelune <shafiqueeee.atif@gmail.com>
2026-02-16 22:24:25 +0800

    13:Added a dynamic product information page that gets the slug of a product and displays its information

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,6 +1,6 @@
 import { CategoryTiles } from "@/components/featuredSections/CategoryTiles";
 import { FeaturedCarousel } from "@/components/featuredSections/FeaturedCarousel";
-import { FeaturedCarouselSkeleton } from "@/components/featuredSections/FeaturedCarouselSkeleton";
+import { FeaturedCarouselSkeleton } from "@/components/app/skeletons/FeaturedCarouselSkeleton";
 import { ProductSection } from "@/components/featuredSections/ProductSection";
 import { ALL_CATEGORIES_QUERY } from "@/lib/sanity/queries/categories";
 import { FEATURED_PRODUCTS_QUERY, FILTER_PRODUCTS_BY_NAME_QUERY, FILTER_PRODUCTS_BY_PRICE_ASC_QUERY, FILTER_PRODUCTS_BY_PRICE_DESC_QUERY, FILTER_PRODUCTS_BY_RELEVANCE_QUERY } from "@/lib/sanity/queries/products";
